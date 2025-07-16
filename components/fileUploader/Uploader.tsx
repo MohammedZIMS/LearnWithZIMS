77a3524f4c40be5e026b95ec1701adf874ea4c87
Md. Zahidul Islam Mehedi <zimss75656@gmail.com>
2025-07-16 21:54:09 +0600

    Update file uploader1

--- components/fileUploader/Uploader.tsx
+++ components/fileUploader/Uploader.tsx
@@ -2,15 +2,66 @@
 
 import { FileRejection, useDropzone } from "react-dropzone";
 import { Card, CardContent } from "../ui/card";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { cn } from "@/lib/utils";
 import { RenderEmptyState, RenderErrorState } from "./RenderState";
 import { toast } from "sonner";
+import { v4 as uuidv4 } from 'uuid';
+
+interface UploaderState {
+    id: string | null;
+    file: File | null;
+    uploading: boolean;
+    progress: number;
+    isDeleting: boolean;
+    key?: string;
+    error: boolean;
+    objectUrl?: string;
+    fileType: "image" | "video" | "pdf" | "doc" ;
+}
 
 
 export function Uploader() {
+
+    const [fileState, setFileState] = useState<UploaderState>({
+        error: false,
+        file: null,
+        id: null,
+        uploading: false,
+        progress: 0,
+        isDeleting: false,
+        fileType: 'image'
+    });
+
+    function uploadFile() {
+        setFileState((pev) => ({
+            ...pev,
+            uploading: true,
+            progress: 0,
+        }));
+
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
     const onDrop = useCallback((acceptedFile :File[]) => {
-        console.log(acceptedFile);
+        if (acceptedFile.length > 0) {
+            const file = acceptedFile[0];
+            
+            setFileState({
+                file: file,
+                uploading: false,
+                progress: 0,
+                objectUrl: URL.createObjectURL(file),
+                error: false,
+                id: uuidv4(),
+                isDeleting: false,
+                fileType: "image"
+            })
+        }
         
     },[]);
 
@@ -49,7 +100,8 @@ export function Uploader() {
             <CardContent className="flex items-center justify-center h-full w-full p-4">
 
             <input {...getInputProps()} />
-            <RenderErrorState/>
+            <RenderEmptyState isDragActive={isDragAccept}/>
+            {/* <RenderErrorState/> */}
             </CardContent>
         </Card>
     )
